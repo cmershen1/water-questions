4a4315fe0c887191f8d99a7742eb84885d4dece6
cmershen1 <cmershen1@gmail.com>
2016-12-09 17:33:01 +0800

    2016.12.9

---
+++ hackerrank/algorithms/warmup/2.simplearraysum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    int n,a,sum=0;
+    scanf("%d",&n);
+    while(n--) {
+        scanf("%d",&a);
+        sum+=a;
+    }
+    printf("%d\n",sum );
+}
---
+++ hackerrank/algorithms/warmup/4.averybigsum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    int n,a;
+    ll sum=0;
+    cin>>n;
+    while(n--) {
+        cin>>a;
+        sum+=a;
+    }
+    cout<<sum<<endl;
+}
