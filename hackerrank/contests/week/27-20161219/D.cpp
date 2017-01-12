8946ea05cedaf7b34d642c57d232547ea85e82ce
cmershen <cmershen1@gmail.com>
2017-01-12 21:15:52 +0800

    2017.1.12

--- hackerrank/contests/week/27-20161219/D.cpp
+++ hackerrank/contests/week/27-20161219/D.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 #define RE(x) freopen(x,"r",stdin)
 #define WR(x) freopen(x,"w",stdout)
-#define INF 0x3f3f3f3f
-#define CLEAR(x) memset(x,0,sizeof(x))
+#define ms(x,y) memset(x,y,sizeof(x))
 #define pb push_back
 #define mp make_pair
+#define INF 0x3f3f3f3f
+#define eps 1e-8
 
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<int,int> pii;
+typedef vi vector<int>;
+typedef pi pair<int,int>;
+typedef vl vector<ll>;
 
-const int M = 1e9+7;
+const int M = 1e9 + 7;
 const double PI = acos(-1.0);
-const double eps = 1e-8;
+
+const int MAXN = 1e5;
+const int MAXM = 1e6;
 
 int main() {
     //RE("in.txt");WR("out.txt");
