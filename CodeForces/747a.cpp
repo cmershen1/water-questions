743b09d6207e9eec22a8d25f1df2ae57c474dce8
cmershen <cmershen1@gmail.com>
2017-05-27 09:33:39 +0800

    5.27

---
+++ CodeForces/747a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=sqrt(n);i>=1;i--) {
+        if(n%i==0) {
+            cout<<i<<' '<<n/i<<endl;
+            return 0;
+        }
+    }
+}
